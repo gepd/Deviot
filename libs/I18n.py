91c1b4aaf02c487f64033e260940c275c5af8e3d
GEPD <guillermoepd@hotmail.com>
2016-01-03 17:21:03 -0300

    Changed files, functions and methods names to maintain convention style

--- libs/I18n.py
+++ libs/I18n.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#
-# Documents
-#
-
-"""
-Documents
-"""
-
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
@@ -17,10 +9,16 @@ from __future__ import unicode_literals
 import os
 import glob
 
-from . import Paths
-from . import Tools
-from .Preferences import Preferences
-from .JSONFile import JSONFile
+try:
+    from . import Paths
+    from . import Tools
+    from .Preferences import Preferences
+    from .JSONFile import JSONFile
+except:
+    from libs import Paths
+    from libs import Tools
+    from libs.Preferences import Preferences
+    from libs.JSONFile import JSONFile
 
 
 @Tools.singleton
@@ -31,32 +29,32 @@ class I18n(object):
         self.lang_ids = []
         self.id_path_dict = {}
         self.trans_dict = {}
-        self.list_ids()
+        self.listIds()
         self.Preferences = Preferences()
-        self.lang_id = self.Preferences.get('lang_id', Tools.getSystemLang())
-        self.change_lang(self.lang_id)
+        self.lang_id = self.Preferences.get('id_lang', Tools.getSystemLang())
+        self.changeLang(self.lang_id)
 
-    def list_ids(self):
+    def listIds(self):
         language_list_path = Paths.getLanguageList()
         self.lang_params = JSONFile(language_list_path).getData()
         language_path = Paths.getLanguagePath()
 
-        lang_file_paths = glob.glob(language_path + '/lang_*.txt')
+        lang_file_paths = glob.glob(language_path + '/*.lang')
         lang_file_names = [os.path.basename(p) for p in lang_file_paths]
-        self.lang_ids += [name[5:-4] for name in lang_file_names]
+        self.lang_ids += [os.path.splitext(nam)[0] for nam in lang_file_names]
         self.id_path_dict.update(dict(zip(self.lang_ids, lang_file_paths)))
         self.lang_ids.sort(key=lambda _id: self.lang_params.get(_id)[1])
 
-    def change_lang(self, lang_id):
+    def changeLang(self, lang_id):
         if lang_id in self.id_path_dict:
             self.lang_id = lang_id
             lang_file_path = self.id_path_dict[lang_id]
             lang_file = LanguageFile(lang_file_path)
-            self.trans_dict = lang_file.get_trans_dict()
+            self.trans_dict = lang_file.getTransDict()
         else:
             self.lang_id = 'en'
             self.trans_dict = {}
-        self.Preferences.set('lang_id', self.lang_id)
+        self.Preferences.set('id_lang', self.lang_id)
 
     def translate(self, text, *params):
         trans_text = self.trans_dict.get(text, text)
@@ -65,13 +63,13 @@ class I18n(object):
             trans_text = trans_text.replace(seq_text, str(param))
         return trans_text
 
-    def get_lang_id(self):
+    def getLangId(self):
         return self.lang_id
 
-    def get_lang_ids(self):
+    def getLangIds(self):
         return self.lang_ids
 
-    def get_lang_names(self, lang_id):
+    def getLangNames(self, lang_id):
         return self.lang_params.get(lang_id, ['Unknown', 'Unknown'])
 
 
@@ -80,25 +78,25 @@ class LanguageFile(JSONFile):
     def __init__(self, path):
         super(LanguageFile, self).__init__(path)
         text = self.readFile()
-        self.trans_dict = load_trans_dict(text)
+        self.trans_dict = loadTransDict(text)
 
-    def get_trans_dict(self):
+    def getTransDict(self):
         return self.trans_dict
 
 
-def load_trans_dict(text):
+def loadTransDict(text):
     trans_dict = {}
     lines = text.split('\n')
     lines = [line.strip() for line in lines if lines if line.strip() and
              not line.strip().startswith('#')]
-    blocks = split_lines(lines)
+    blocks = splitLines(lines)
     for block in blocks:
-        key, value = load_trans_pair(block)
+        key, value = loadTransPair(block)
         trans_dict[key] = value
     return trans_dict
 
 
-def split_lines(lines):
+def splitLines(lines):
     block, blocks = [], []
     for line in lines:
         if line.startswith('msgid'):
@@ -110,7 +108,7 @@ def split_lines(lines):
     return blocks
 
 
-def load_trans_pair(block):
+def loadTransPair(block):
     is_key, key, value = True, '', ''
     for line in block:
         index = line.index('"')
