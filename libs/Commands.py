e5ec36bdc66d0b837cf2991653708d2bf0c45456
GEPD <guillermoepd@gmail.com>
2016-01-08 14:50:28 -0300

    Added realtime error output when it building a project

--- libs/Commands.py
+++ libs/Commands.py
@@ -8,6 +8,7 @@ from __future__ import unicode_literals
 
 import subprocess
 import os
+import time
 
 try:
     from . import Messages
@@ -48,6 +49,18 @@ class CommandsPy(object):
         except:
             args = ''
 
+        # output errors only
+        if('run' == options and not verbose):
+            args += '-v --verbose'
+
+        cmd_type = self.getTypeAction(commands)
+
+        # Console message
+        current_time = time.strftime('%H:%M:%S')
+        start_time = time.time()
+        self.message_queue.put(cmd_type, current_time)
+
+        # run command
         command = "platformio -f -c sublimetext %s %s 2>&1" % (
             options, args)
 
@@ -55,33 +68,78 @@ class CommandsPy(object):
                                    stdout=subprocess.PIPE, cwd=self.cwd,
                                    universal_newlines=True, shell=True)
 
-        """ REALTIME OUTPUT TEST """
-        if('run' in command):
+        # real time error build output
+        if('-v --verbose' in command and not verbose):
+            err = False
             while True:
                 output = process.stdout.readline()
                 if output == '' and process.poll() is not None:
+                    if(err):
+                        current_time = time.strftime('%H:%M:%S')
+                        diff_time = time.time() - start_time
+                        diff_time = '{0:.2f}'.format(diff_time)
+                        msg = '\\n{0} it took {1}s\\n'
+                        self.message_queue.put(msg, current_time, diff_time)
                     break
-                if output:
-                    self.message_queue.put(output.strip() + '\n')
-        """ REALTIME OUTPUT TEST END """
+
+                # detect error
+                if('in function' in output.lower() or
+                        'in file' in output.lower() or
+                        'error:' in output.lower() and not err):
+                    err = True
+                    current_time = time.strftime('%H:%M:%S')
+                    msg = 'Error\\n{0} Details:\\n\\n'
+                    self.message_queue.put(msg, current_time)
+
+                # output messages
+                if (output.strip() and err and 'scons' not in output and
+                        'platform' not in output.lower() and
+                        'took' not in output.lower() and
+                        '....' not in output and not
+                        '.' == output.strip() and
+                        'exit status' not in output.lower()):
+                    self.message_queue.put(output)
 
         output = process.communicate()
-
         stdout = output[0]
         stderr = output[1]
-
         return_code = process.returncode
 
+        # Print success status
+        if(not verbose and return_code == 0):
+            current_time = time.strftime('%H:%M:%S')
+            diff_time = time.time() - start_time
+            diff_time = '{0:.2f}'.format(diff_time)
+            msg = 'Success\\n{0} it took {1}s\\n'
+            self.message_queue.put(msg, current_time, diff_time)
+
+        # print full verbose output
         if(verbose):
             self.message_queue.put(stdout)
 
             if(stderr):
                 self.message_queue.put(stderr)
 
-        if(return_code > 0):
-            self.error_running = True
-            if(not setReturn):
-                return stdout
-
+        # return output
         if(setReturn):
             return stdout
+
+    def getTypeAction(self, command):
+        """
+        Get the type of action, to get the header
+        and print it in the user console
+
+        Arguments:
+            command {string} -- CLI command
+        """
+        if 'init' in command:
+            return '{0} Initializing the project | '
+        elif '-t upload' in command:
+            return '{0} Uploading firmware | '
+        elif '-t clean' in command:
+            return '{0} Cleaning built files | '
+        else:
+            return '{0} Building the project | '
+
+    def __del__(self):
+        self.message_queue.stopPrint()
