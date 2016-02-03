cb088575d42b97113be68e6032a4ebf1ef5896c3
GEPD <guillermoepd@gmail.com>
2016-02-02 23:16:03 -0300

    Added the creation of completions and syntax files

--- libs/Tools.py
+++ libs/Tools.py
@@ -7,6 +7,7 @@ from __future__ import unicode_literals
 
 import os
 import sys
+import glob
 import locale
 import sublime
 
@@ -262,3 +263,131 @@ def closeSerialMonitors(preferences):
                 preferences.set('autorun_monitor', True)
                 cur_serial_monitor.stop()
             monitor_module.serials_in_use.remove(port)
+
+
+def getKeywords():
+    try:
+        from . import Paths
+        from . import Keywords
+    except:
+        from libs import Paths
+        from libs import Keywords
+
+    keywords_dirs = []
+    keywords = []
+
+    # User Library
+    user_lib_path = Paths.getUserLibraryPath()
+    user_lib_path = os.path.join(user_lib_path, '*')
+
+    # Platformio Libraries
+    pio_lib_path = Paths.getPioLibrary()
+    pio_lib_path = os.path.join(pio_lib_path, '*')
+
+    # Core Paths
+    pio_packages = Paths.getPioPackages()
+    pio_packages = os.path.join(pio_packages, '*')
+    sub_dirs = glob.glob(pio_packages)
+    for path in sub_dirs:
+        sub_paths = glob.glob(path)
+        for sub_path in sub_paths:
+            sub_path = os.path.join(sub_path, '*')
+            sub_path = glob.glob(sub_path)
+            for core_lib in sub_path:
+                if 'libraries' in core_lib:
+                    lib = os.path.join(core_lib, '*')
+                    keywords_dirs.append(lib)
+
+    keywords_dirs += [user_lib_path, pio_lib_path]
+
+    for path in keywords_dirs:
+        sub_dirs = glob.glob(path)
+        for sub_dir in sub_dirs:
+            key_file = os.path.join(sub_dir, 'keywords.txt')
+            if(os.path.isfile(key_file)):
+                keywords.append(Keywords.KeywordsFile(key_file))
+    return keywords
+
+
+def createCompletions():
+    try:
+        from . import Paths
+        from .JSONFile import JSONFile
+    except:
+        from libs import Paths
+        from libs.JSONFile import JSONFile
+
+    keywords = getKeywords()
+    keyword_ids = []
+    user_path = Paths.getDeviotUserPath()
+    completion_path = os.path.join(user_path, 'Deviot.sublime-completions')
+
+    cpp_keywords = ['define', 'error', 'include', 'elif', 'endif']
+    cpp_keywords += ['ifdef', 'ifndef', 'undef', 'line', 'pragma']
+
+    for k in keywords:
+        for w in k.get_keywords():
+            keyword_ids += [w.get_id() for w in k.get_keywords()]
+
+    keyword_ids = set(keyword_ids)
+    keyword_ids = [word for word in keyword_ids]
+
+    completions_dict = {'scope': 'source.arduino'}
+    completions_dict['completions'] = keyword_ids
+
+    file = JSONFile(completion_path)
+    file.setData(completions_dict)
+
+
+def createSyntaxFile():
+    try:
+        from . import Paths
+        from .JSONFile import JSONFile
+    except:
+        from libs import Paths
+        from libs.JSONFile import JSONFile
+
+    keywords = getKeywords()
+
+    LITERAL1s = []
+    KEYWORD1s = []
+    KEYWORD2s = []
+    KEYWORD3s = []
+
+    # set keywords
+    for k in keywords:
+        for w in k.get_keywords():
+            if 'LITERAL1' in w.get_type():
+                LITERAL1s.append(w.get_id())
+            if 'KEYWORD1' in w.get_type():
+                KEYWORD1s.append(w.get_id())
+            if 'KEYWORD2' in w.get_type():
+                KEYWORD2s.append(w.get_id())
+            if 'KEYWORD3' in w.get_type():
+                KEYWORD3s.append(w.get_id())
+
+    # formating
+    LITERAL1s = set(LITERAL1s)
+    LITERAL1s = '|'.join(LITERAL1s)
+    KEYWORD1s = set(KEYWORD1s)
+    KEYWORD1s = '|'.join(KEYWORD1s)
+    KEYWORD2s = set(KEYWORD2s)
+    KEYWORD2s = '|'.join(KEYWORD2s)
+    KEYWORD3s = set(KEYWORD3s)
+    KEYWORD3s = '|'.join(KEYWORD3s)
+
+    # get sintax preset
+    sintax_path = Paths.getSyntaxPath()
+    sintax_file = JSONFile(sintax_path)
+    sintax = sintax_file.readFile()
+
+    # replace words in sintax file
+    sintax = sintax.replace('${LITERAL1}', LITERAL1s)
+    sintax = sintax.replace('${KEYWORD1}', KEYWORD1s)
+    sintax = sintax.replace('${KEYWORD2}', KEYWORD2s)
+    sintax = sintax.replace('${KEYWORD3}', KEYWORD3s)
+
+    # Save File
+    file_path = Paths.getTmLanguage()
+    language_file = JSONFile(file_path)
+    language_file.writeFile(sintax)
