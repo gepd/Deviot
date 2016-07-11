5424c95f24a498f0bbab37517e29fb1a71b9df5f
GEPD <guillermoepd@gmail.com>
2016-07-10 23:11:19 -0400

    New restart command

--- DeviotStarter.py
+++ DeviotStarter.py
@@ -598,3 +598,29 @@ class AddStatusCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, text, erase_time):
         Tools.setStatus(text, erase_time)
+
+
+class SublimeRestartCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        if sys.platform == 'win32':
+            if sublime.version()[:1] == '3':
+                subprocess.call('taskkill /im sublime_text.exe /f && cmd /C "' +
+                                os.path.join(os.getcwd(), 'sublime_text.exe') + '"', shell=True)
+            else:
+                os.execl(sys.executable, ' ')
+        elif sys.platform == 'darwin':
+            # Restarting ST3 on mac
+            if sublime.version()[:1] == '3':
+                subprocess.call("pkill subl && " +
+                                os.path.join(os.getcwd(), 'subl'), shell=True)
+            else:
+                os.execl(os.path.join(os.getcwd(), 'subl'))
+        else:
+            # Restarting ST3 on linux
+            if sublime.version()[:1] == '3':
+                subprocess.call("pkill 'sublime_text' && " +
+                                os.path.join(os.getcwd(), 'sublime_text'), shell=True)
+
+            else:
+                os.execl(os.path.join(os.getcwd(), 'sublime_text'))
