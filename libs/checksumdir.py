1e8368888ab0eabf52f5b533173c61c0152ad07c
GEPD <guillermoepd@hotmail.com>
2016-08-11 18:10:33 -0400

    Bug fix libraries not refreshing (Issue https://github.com/gepd/Deviot/issues/49)

--- libs/checksumdir.py
+++ libs/checksumdir.py
@@ -1,60 +1,28 @@
-"""
-Function for deterministically creating a single hash for a directory of files,
-taking into account only file contents and not filenames.
+def dirhash(directory, verbose=0):
+  import hashlib, os
+  SHAhash = hashlib.md5()
+  if not os.path.exists (directory):
+    return -1
 
-Usage:
-
-from checksumdir import dirhash
-
-dirhash('/path/to/directory', 'md5')
-
-"""
-
-import os
-import hashlib
-import re
-
-__version__ = '1.0.5'
-
-
-HASH_FUNCS = {
-    'md5': hashlib.md5,
-    'sha1': hashlib.sha1,
-    'sha256': hashlib.sha256,
-    'sha512': hashlib.sha512
-}
-
-
-def dirhash(dirname, hashfunc='md5'):
-    hash_func = HASH_FUNCS.get(hashfunc)
-    if not hash_func:
-        raise NotImplementedError('{} not implemented.'.format(hashfunc))
-
-    if not os.path.isdir(dirname):
-        raise TypeError('{} is not a directory.'.format(dirname))
-    hashvalues = []
-    for root, dirs, files in os.walk(dirname, topdown=True):
-        if not re.search(r'/\.', root):
-            hashvalues.extend([_filehash(os.path.join(root, f),
-                                         hash_func) for f in files if not
-                               f.startswith('.') and not re.search(r'/\.', f)])
-    return _reduce_hash(hashvalues, hash_func)
+  try:
+    for root, dirs, files in os.walk(directory):
+      for names in files:
+        if verbose == 1:
+            print (b'Hashing', names)
+        filepath = os.path.join(root,names)
+        with open(filepath, 'rb') as f1:
+            while True:
+                buf = f1.read(4096)
+                if not buf : break
+                SHAhash.update(buf)
+                SHAhash.hexdigest()           
+  except:
+    import traceback
+    # Print the stack traceback
+    traceback.print_exc()
+    return -2
+    
+  return SHAhash.hexdigest()
 
 
-def _filehash(filepath, hashfunc):
-    hasher = hashfunc()
-    blocksize = 64 * 1024
-    with open(filepath, 'rb') as fp:
-        while True:
-            data = fp.read(blocksize)
-            if not data:
-                break
-            hasher.update(data)
-    return hasher.hexdigest()
-
-
-def _reduce_hash(hashlist, hashfunc):
-    hasher = hashfunc()
-    for hashvalue in sorted(hashlist):
-        hasher.update(hashvalue.encode('utf-8'))
-    return hasher.hexdigest()
+ 
