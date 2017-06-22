a2813fc0c7d6465cdbe02ba50c595debbad47e4c
gepd <guillermoepd@hotmail.com>
2017-06-22 16:52:22 -0400

    command to set the OTA password

---
+++ commands/deviot_set_password.py
@@ -0,0 +1,21 @@
+from sublime_plugin import WindowCommand
+from ..libraries.tools import get_setting, save_setting
+
+class DeviotSetPasswordCommand(WindowCommand):
+    """
+    Stores the password to use in OTA Upload
+
+    """
+
+    def run(self):
+        from ..libraries.I18n import I18n
+
+        _ = I18n().translate
+
+        caption = _("pass_caption")
+        self.window.show_input_panel(caption, '', self.on_done, None, None)
+
+    def on_done(self, password):
+        save_setting('auth_pass', password)
+        if(get_setting('last_action', None)):
+            self.window.run_command("deviot_upload_sketch")
