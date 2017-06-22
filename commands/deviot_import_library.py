b9c9d7ec4bc545a9417d0d6bd9b264240ee68c64
gepd <guillermoepd@hotmail.com>
2017-06-22 12:08:54 -0400

    Avoid to import a library header in a non IOT file

--- commands/deviot_import_library.py
+++ commands/deviot_import_library.py
@@ -1,6 +1,11 @@
 from sublime_plugin import WindowCommand
 from ..libraries.quick_menu import QuickMenu
+from ..libraries.project_check import ProjectCheck
 
 class DeviotImportLibraryCommand(WindowCommand):
     def run(self):
         QuickMenu().quick_import()
+
+    def is_enabled(self):
+        is_iot = ProjectCheck().is_iot()
+        return bool(is_iot)
