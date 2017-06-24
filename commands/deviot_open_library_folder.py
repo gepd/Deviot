1ad8eccf15961478ac2dab976406bbdf4ae4d958
gepd <guillermoepd@hotmail.com>
2017-06-24 10:01:37 -0400

    Bug fix trying to open the library folder when it's not created yet

--- commands/deviot_open_library_folder.py
+++ commands/deviot_open_library_folder.py
@@ -1,6 +1,8 @@
 from sublime_plugin import TextCommand
 from sublime import run_command
 
+from os import path
+from ..libraries.tools import make_folder
 from ..libraries.paths import getPioLibrary
 
 class DeviotOpenLibraryFolderCommand(TextCommand):
@@ -12,4 +14,8 @@ class DeviotOpenLibraryFolderCommand(TextCommand):
 
     def run(self, edit):
         pio_library = getPioLibrary()
+        
+        if(not path.exists(pio_library)):
+            make_folder(pio_library)
+
         run_command('open_url', {'url': pio_library})
