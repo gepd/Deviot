gepd/Deviot

commit 284f5db93d28ca156589f62a7aea3b6145342342

Fix the config file if this hasn't the "config" header

libraries/tools.py
@@ -118,7 +118,20 @@ def get_sysetting(key, default=None):
     section = "config"
     sys_path = getSystemIniPath()
     config = configparser.RawConfigParser()
-    config.read(sys_path)
+
+    try:
+        config.read(sys_path)
+    except:
+        d_conf = ''
+        
+        with open(sys_path) as file:
+            d_conf = file.read()
+
+        with open(sys_path, 'w+') as file:
+            d_conf = '[config]\n' + d_conf
+            file.write(d_conf)
+
+        config.read(sys_path)
     
     if(not config.has_option(section, key)):
         return default

commit 4681d2a10800b1417bcfd80918ed938f3e9fc4e2

fix bug introduced in: 284f5db

libraries/tools.py
@@ -130,6 +130,7 @@ def get_sysetting(key, default=None):
         d_header = '[config]\n'
 
         if(d_header not in d_conf):
+            d_conf = d_header + d_conf
             with open(sys_path, 'w+') as file:
                 file.write(d_conf)
 