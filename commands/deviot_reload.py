acbe0d4e396043a82da45c316c799783166c4058
DESKTOP-36KV42A\guill <guillermoepd@hotmail.com>
2017-09-10 22:11:03 -0300

    Fix package name string in reload plugin command

--- commands/deviot_reload.py
+++ commands/deviot_reload.py
@@ -3,11 +3,13 @@ import sublime_plugin
 
 from ..libraries.progress_bar import ProgressBar
 from ..libraries.reloader import reload_package
+from ..libraries.paths import getPluginName
 
+NAME = getPluginName()
 
 class DeviotReloadCommand(sublime_plugin.WindowCommand):
 
-    def run(self, pkg_name='Deviot'):
+    def run(self, pkg_name=NAME):
         sublime.set_timeout_async(lambda: self.run_async(pkg_name))
 
     def run_async(self, pkg_name=None):
