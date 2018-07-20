0520ee03db2467f79e6e6e5d5dd1e7a5bca8c753
gepd <guillermoepd@hotmail.com>
2018-07-20 18:25:51 -0400

    minor syntax changes

--- libraries/menu_files.py
+++ libraries/menu_files.py
@@ -13,56 +13,54 @@ from .file import File
 from ..platformio.pio_bridge import PioBridge
 from .I18n import I18n
 
-_ = None
 
 class MenuFiles(PioBridge):
     def __init__(self):
         super(MenuFiles, self).__init__()
-        global _
-        _ = I18n().translate
+        self.translate = I18n().translate
 
     def get_template_menu(self, file_name):
         """Template Menu
-        
+
         Get a JSON template file and return it.
         To this option works, the file must be located
         in the Deviot/presets folder
-        
+
         Arguments:
             file_name {str} -- name of the file
-        
+
         Returns:
             json -- data of the file
         """
         file_path = paths.getPresetFile(file_name)
         preset_file = File(file_path)
         preset_data = preset_file.read_json()
-        
+
         return preset_data
 
     def create_sublime_menu(self, data, menu_name, path):
         """Sublime Menu
-        
+
         Generate a .sublime-menu file who is used by sublime text
         to generate the menu in the menu bar.
-        
+
         Arguments:
             data {json} -- data with the structure of the menu
             menu_name {str} -- the file will be called menu_name.sublime-menu
             path {str} -- where the menu will be located
         """
         from json import dumps
-        
+
         menu_name = menu_name + '.sublime-menu'
         menu_path = os.path.join(path, menu_name)
-        
+
         file = File(menu_path)
         data = dumps(data, sort_keys=True, indent=4)
         file.write(data)
 
     def create_quick_commands(self):
         """Quick Commands
-        
+
         Makes the quick command file and translate it
         to the current language selected
         """
@@ -74,14 +72,14 @@ class MenuFiles(PioBridge):
         quick_json = quick_json.read_json()
 
         for items in quick_json:
-            items['caption'] = "Deviot: " + _(items['caption'])
+            items['caption'] = "Deviot: " + self.translate(items['caption'])
 
         quick_file = File(output_path)
         quick_file.save_json(quick_json)
 
     def create_context_menu(self):
         """Quick Commands
-        
+
         Makes the quick command file and translate it
         to the current language selected
         """
@@ -93,8 +91,7 @@ class MenuFiles(PioBridge):
         context_json = context_json.read_json()
 
         for items in context_json:
-            items['caption'] = _(items['caption'])
+            items['caption'] = self.translate(items['caption'])
 
         context_file = File(output_path)
         context_file.save_json(context_json)
-
