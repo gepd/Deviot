19461ac21ab330a830fa5887fa6415f8ff2ec23a
gepd <guillermoepd@hotmail.com>
2018-07-21 20:21:09 -0400

    minor syntax changes

--- platformio/project_recognition.py
+++ platformio/project_recognition.py
@@ -4,16 +4,11 @@
 """
 This module read all the information related with the project/file open:
 
-File Path, Project Path (Same Path Without filename), Parent Path, File Name, 
+File Path, Project Path (Same Path Without filename), Parent Path, File Name,
 File Extension, Temp Path, if the project is initialized, the path of the
 platformio.ini file, if it's native (platformio structure or not), list of
 environments initialized and if the src_dir flag is set.
 
-You can call to the "ProjectDetails" class and it will show all the information
-available. This code is intended to work as a standalone so you can call it from
-the sublime console and see the results.
-
-Version: 1.0.0
 Author: Guillermo Díaz
 Contact: gepd@outlook.com
 Licence: Same as the project (Read the LICENCE file in the root)
@@ -28,6 +23,7 @@ import os
 import sublime
 from ..libraries.tools import get_setting
 
+
 class ProjectRecognition(object):
     def __init__(self):
         self.window = sublime.active_window()
@@ -36,12 +32,12 @@ class ProjectRecognition(object):
 
     def get_file_name(self):
         """Window File Name
-        
+
         Gets the name of window, if the user has open a common file
         it will take the file name, but in the case of a new window
         the name will be null, or you can change that name using
         the method set_name of the view.
-        
+
         Returns:
             str -- File name otherwise null
         """
@@ -49,25 +45,25 @@ class ProjectRecognition(object):
 
     def get_file_path(self):
         """File Path
-        
+
         Full path of the file loaded in the current view,
         including the file name
-        
+
         Returns:
             [str] -- path of the file path/path/file.ext
         """
         freeze = get_setting('freeze_sketch', None)
-        
+
         if(freeze):
             return freeze
         return self.view.file_name()
 
     def get_project_path(self):
         """Project Path
-        
-        Path (without file name) from the current file loaded 
+
+        Path (without file name) from the current file loaded
         in the current view.
-        
+
         Returns:
             [str] -- path of the file path/path/
         """
@@ -85,7 +81,6 @@ class ProjectRecognition(object):
         Path of project in a temporal folder, this folder
         do not neccessarilly exits
 
-        
         Returns:
             [str] -- temp_path/project_name/
         """
@@ -97,7 +92,7 @@ class ProjectRecognition(object):
         ext = self.get_file_extension()
         if(ext and ext != 'ino'):
             from glob import glob
-            
+
             project_path = self.get_project_path()
             project_path = os.path.join(project_path, '*')
             project_path = glob(project_path)
@@ -113,11 +108,11 @@ class ProjectRecognition(object):
 
     def get_parent_path(self):
         """Parent Path
-        
+
         Parent path or one folder behind of the file currently
-        loaded in the current  view the path do not include 
+        loaded in the current  view the path do not include
         the file name
-        
+
         Returns:
             [str] -- path/parent_path/
         """
@@ -131,16 +126,16 @@ class ProjectRecognition(object):
 
     def get_file_name(self, custom_path=None, ext=True):
         """File Name
-        
-        Name of the file loaded in the current view. 
-        
+
+        Name of the file loaded in the current view.
+
         Keyword Arguments:
             custom_path {str} -- custom path to get the file name
 
-            ext {bool} -- if is set to false removes the 
-                          extension of the filename 
+            ext {bool} -- if is set to false removes the
+                          extension of the filename
                           (default: {True})
-        
+
         Returns:
             [str] -- filename.ext or filename
         """
@@ -160,10 +155,10 @@ class ProjectRecognition(object):
 
     def get_file_extension(self):
         """Extension
-        
+
         Extract the extension of the file loaded in the current
         view and return it (without the dot)
-        
+
         Returns:
             [str] -- ext
         """
@@ -177,9 +172,9 @@ class ProjectRecognition(object):
 
     def get_file_hash(self):
         """Hash File Path
-        
+
         Unique hash based in the file path
-        
+
         Returns:
             str -- hash of the file path
         """
@@ -201,11 +196,11 @@ class ProjectRecognition(object):
         files are stored in other folder, in this case this
         folder is located in the temporal folder of your current
         operative system
-        
+
         platformio.ini is searched in the parent folder or in
         the temp folder and return the path, if the file is not
         found in any of this folders it will returns None
-        
+
         Returns:
             [str/none] -- path/platformio.ini / none
         """
@@ -218,10 +213,10 @@ class ProjectRecognition(object):
             return None
 
         ini_path = self.search_pio_ini(parent)
-        
+
         if(not ini_path and pio_structure):
             return None
-                
+
         if(not ini_path):
             temp = self.get_temp_project_path()
             ini_path = self.search_pio_ini(temp)
@@ -230,12 +225,12 @@ class ProjectRecognition(object):
 
     def get_envs_initialized(self):
         """Initialized Environments
-        
+
         List with all the available (initialized) environments in
         the platformio.ini file of the current file. If the file did
         not exits or there are none environment initialized it will
         return none
-        
+
         Returns:
             [list/none] -- [environment, environment] / none
         """
@@ -257,14 +252,13 @@ class ProjectRecognition(object):
 
         return environments
 
-
     def get_src_dir(self):
         """SRC DIR
-        
+
         Check if the src_dir flag has been set in the platformio.ini,
-        if it has been set, will return the value otherwise it will 
+        if it has been set, will return the value otherwise it will
         return none
-        
+
         Returns:
             [str/none] -- src_dir_path/none
         """
@@ -283,10 +277,10 @@ class ProjectRecognition(object):
 
     def is_initialized(self):
         """Project Initialized
-        
+
         True if the platformio.ini file is found in the parent
         folder or in the temporal folder, false if not
-        
+
         Returns:
             bool -- true if it's found
         """
@@ -297,11 +291,11 @@ class ProjectRecognition(object):
 
     def is_native(self):
         """Native Project
-        
+
         When the platformio.ini file is located in the temp folder
         the project is not native, otherwise it's native, if it's
         not initialized (file nonexistent file) return None
-        
+
         Returns:
             [bool/none] -- True if is native/none if the file not exists
         """
@@ -319,13 +313,13 @@ class ProjectRecognition(object):
 
     def search_pio_ini(self, path):
         """Search platformio.ini
-        
+
         Iterates over the given path and search the platformio.ini file
         if the file is found return the full path of the file otherwise None
-        
+
         Arguments:
             path {str} -- string with the path to search platformio.ini
-        
+
         Returns:
             [str/none] -- path/platformio.ini / none
         """
@@ -337,13 +331,13 @@ class ProjectRecognition(object):
 
     def get_temp_path(self, extra_str=''):
         """Temp Path
-        
+
         Path of the temp folder, if the extra_str argument
         is given it will add that string to the final path
-        
+
         Keyword Arguments:
             extra_str {str} --extring to add at the temp path  (default: '')
-        
+
         Returns:
             [str] -- temp_path | temp_path/extra_str
         """
@@ -364,33 +358,3 @@ class ProjectRecognition(object):
             tmp_path = os.path.join(tmp_path, extra_str)
 
         return tmp_path
-
-
-class ProjectDetails(ProjectRecognition):
-    """Details
-    
-    Show all the information related with the current project/file
-    it includes platformIO information (platformio.ini)
-    
-    Extends:
-        ProjectRecognition
-    """
-    def __init__(self):
-        super(ProjectDetails, self).__init__()
-
-        width = 25
-        
-        details = ["File Path", "Project Path", "Paren Path",
-                    "File Name", "File Extension", "Temp Path",
-                    "Ini Path", "Initialized", "Native",
-                    "Envs Initialized", "SRC-DIR"]
-        
-        infos = [self.get_file_path(), self.get_project_path(),
-                self.get_parent_path(), self.get_file_name(),
-                self.get_file_extension(), self.get_temp_path(),
-                self.get_ini_path(), self.is_initialized(),
-                self.is_native(), self.get_envs_initialized(),
-                self.get_src_dir()]
-
-        for detail, info in zip(details, infos):
-            print ("{}: {}".format(detail.ljust(width), str(info).ljust(width)))
