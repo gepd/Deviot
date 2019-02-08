0042e51b687572eb134a60407e3d87ac257af79b
gepd <guillermoepd@hotmail.com>
2019-02-07 21:11:37 -0300

    Fix to avoid AttributeError when an extra library folder is set https://github.com/gepd/Deviot/issues/271

--- api/deviot.py
+++ api/deviot.py
@@ -554,13 +554,13 @@ def globalize(route):
     return glob(route)
 
 
-def folder_explorer(path=None, callback=None, key=None, plist=None, index=-2):
+def folder_explorer(pathstr=None, callback=None, key=None, plist=None, index=-2):
     """Explore a path
 
     Using the quick panel, this fuction allows the user to select a path, it
     will be always a folder.
 
-    When you give a path in the 'path' argument, the explorer will be open it
+    When you give a path in the 'pathstr' argument, the explorer will be open it
     in the given path. If you don't pass any path, the 'last_path' setting
     will be check to open the explorer in the last path used. If not path is
     found it will show the root path.
@@ -574,7 +574,7 @@ def folder_explorer(path=None, callback=None, key=None, plist=None, index=-2):
     about it
 
     Keyword Arguments:
-        path {str} -- stores the current path selected (default: None)
+        pathstr {str} -- stores the current path selected (default: None)
         callback {function} -- called when a folder is selected (default: None)
         key {str} -- key to use in the callback (default: {None})
         plist {list} -- list of path handled by the function (default: None)
@@ -588,13 +588,13 @@ def folder_explorer(path=None, callback=None, key=None, plist=None, index=-2):
         return
 
     # close if can't back anymore
-    if(not path and index == 1):
+    if(not pathstr and index == 1):
         return
 
     # last path used
-    if(not path):
+    if(not pathstr):
         from ..libraries.tools import get_setting
-        path = get_setting('last_path', None)
+        pathstr = get_setting('last_path', None)
 
     from ..libraries.I18n import I18n
     _ = I18n().translate
@@ -602,38 +602,38 @@ def folder_explorer(path=None, callback=None, key=None, plist=None, index=-2):
     paths_list = []
 
     # recognize path
-    if(path and not plist):
+    if(pathstr and not plist):
         index = -3
-        new_path = globalize(path)
+        new_path = globalize(pathstr)
         paths_list.extend(new_path)
 
     # back
-    if(index == 1 and path):
-        plist = globalize(path)
-        prev = path.dirname(path)
+    if(index == 1 and pathstr):
+        plist = globalize(pathstr)
+        prev = path.dirname(pathstr)
         back_list = globalize(prev)
-        if(path == prev):
+        if(pathstr == prev):
             index = -2
-            path = None
+            pathstr = None
             plist = None
         else:
             paths_list.extend(back_list)
-            path = prev
+            pathstr = prev
 
     # select current
     if(index == 0):
         # store last path used
         from ..libraries.tools import save_setting
-        save_setting('last_path', path)
+        save_setting('last_path', pathstr)
 
         if(not key):
-            return callback(path)
-        return callback(key, path)
+            return callback(pathstr)
+        return callback(key, pathstr)
 
     if(plist and index != 1):
-        path = plist[index]
+        pathstr = plist[index]
 
-    path_caption = path if(path) else "0"
+    path_caption = pathstr if(pathstr) else "0"
     paths_list.insert(0, _("select_{0}", path_caption))
     paths_list.insert(1, _("_previous"))
 
@@ -651,4 +651,4 @@ def folder_explorer(path=None, callback=None, key=None, plist=None, index=-2):
     sublime.set_timeout(
         lambda: quick_panel(
             paths_list, lambda index: folder_explorer(
-                path, callback, key, paths_list, index)), 0)
+                pathstr, callback, key, paths_list, index)), 0)
