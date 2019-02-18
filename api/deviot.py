3be4b5027cd94b22beeea86869ede1a17f321356
gepd <guillermoepd@hotmail.com>
2019-02-18 16:54:05 -0300

    bump to dev11

--- api/deviot.py
+++ api/deviot.py
@@ -45,7 +45,7 @@ def version():
     """
     return the plugin version
     """
-    version = (2, 3, 0, '.dev10')
+    version = (2, 3, 0, '.dev11')
     v_format = ".".join([str(s) for s in version[:3]])
     if(len(version) > 3):
         v_format += version[3]
